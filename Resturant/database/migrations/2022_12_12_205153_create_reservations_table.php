e8f57abfd08327ea5c91b4914b452eee8cf157e5
RezaRfahi <refahireza82@gmail.com>
2022-12-13 00:32:12 +0330

    add reservation migration

---
+++ Resturant/database/migrations/2022_12_12_205153_create_reservations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->unsignedBigInteger('resturant_id');
+            $table->foreign('resturant_id')->references('id')->on('resturants')->onDelete('cascade');
+            $table->string('user_name')->fulltext();
+            $table->string('resturant_name')->fulltext();
+            $table->string('reservation_code')->unique();
+            $table->dateTime('reservation_start_time')->unique();
+            $table->dateTime('reservation_finish_time')->unique();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
