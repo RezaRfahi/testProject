ff1395719570d19b311f9ee82f360674d73c5ab6
RezaRfahi <refahireza82@gmail.com>
2022-12-13 19:21:53 +0330

    all databases are OK

--- Resturant/database/migrations/2022_12_12_205153_create_reservations_table.php
+++ Resturant/database/migrations/2022_12_12_205153_create_reservations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('reservations', function (Blueprint $table) {
-            $table->id();
+//            $table->id();
             $table->unsignedBigInteger('user_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->unsignedBigInteger('resturant_id');
